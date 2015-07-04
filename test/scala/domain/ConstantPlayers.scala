52c053d5e738cc5a31a64759379f2c10e2d69132
Remus Amalinei <remus.amalinei@gmail.com>
2015-07-04 01:34:07 +0100

    Replace fixture with constants in tests

---
+++ test/scala/domain/ConstantPlayers.scala
@@ -0,0 +1,12 @@
+package domain
+
+/**
+ * @author Remus Amalinei
+ */
+object ConstantPlayers {
+  val Cabbage = Player("cabbage")()
+  val Goat = Player("goat")(injuredPlayerSet = Set(Cabbage))
+  val Wolf = Player("wolf")(injuredPlayerSet = Set(Goat))
+  val Bear = Player("bear")(injuredPlayerSet = Set(Goat))
+  val Farmer = Player("farmer")(defendedPlayerMap = Map(Goat -> Wolf, Cabbage -> Goat))
+}
--- test/scala/domain/PlayerTest.scala
+++ test/scala/domain/PlayerTest.scala
@@ -1,5 +1,6 @@
 package domain
 
+import domain.ConstantPlayers._
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FlatSpec, Matchers}
@@ -10,35 +11,19 @@ import org.scalatest.{FlatSpec, Matchers}
 @RunWith(classOf[JUnitRunner])
 class PlayerTest extends FlatSpec with Matchers {
 
-  def fixture =
-    new {
-      val goat = Player("goat")()
-      val wolf = Player("wolf")(injuredPlayerSet = Set(goat))
-      val bear = Player("bear")(injuredPlayerSet = Set(goat))
-      val farmer = Player("farmer")(defendedPlayerMap = Map(goat -> wolf))
-    }
-
   "A Player" should "injure a player" in {
-    val f = fixture
-
-    f.wolf injures (f.goat) should be (true)
+    Wolf injures (Goat) should be (true)
   }
 
   it should "defend a player" in {
-    val f = fixture
-
-    f.farmer defendsPlayerAgainstInjurer (f.goat, f.wolf) should be (true)
+   Farmer defendsPlayerAgainstInjurer (Goat, Wolf) should be (true)
   }
 
   it should "fail to defend another player" in {
-    val f = fixture
-
-    f.farmer defendsPlayerAgainstInjurer (f.goat, f.bear) should be (false)
+    Farmer defendsPlayerAgainstInjurer (Goat, Bear) should be (false)
   }
 
   it should "not both injure and defend the same player" in {
-    val f = fixture
-
-    an [IllegalArgumentException] should be thrownBy Player("test")(Set(f.goat), Map(f.goat -> f.wolf))
+    an [IllegalArgumentException] should be thrownBy Player("test")(Set(Goat), Map(Goat -> Wolf))
   }
 }
