5d37984b38cf9dee899d0d2469d995878e6b7732
Remus Amalinei <remus.amalinei@gmail.com>
2015-10-19 20:56:51 +0100

    Implement TopRankerService
    Project: top-ranker

--- top-ranker/src/main/scala/ra/topranker/application/TopRankerService.scala
+++ top-ranker/src/main/scala/ra/topranker/application/TopRankerService.scala
@@ -1,7 +1,26 @@
 package ra.topranker.application
 
+import ra.topranker.actor.TopRankerActor.{AddScore, QueryTopScores, TopScores}
+import ra.topranker.domain.Score
+
+import scala.concurrent.Future
+
+import akka.actor.ActorRef
+import akka.pattern.ask
+import akka.util.Timeout
+
 /**
  * @author Remus Amalinei
  */
-trait TopRankerService {
+class TopRankerService(topRankerActor: ActorRef) {
+
+  def addScore(score: Score): Unit = {
+    topRankerActor ! AddScore(score)
+  }
+
+  def retrieveTopScores(t: Timeout): Future[TopScores] = {
+    implicit val timeout = t
+
+    (topRankerActor ? QueryTopScores()).mapTo[TopScores]
+  }
 }
