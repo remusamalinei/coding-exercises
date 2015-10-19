3adc154b9cc9317253d3c16d2eb9c0d994f5e6f0
Remus Amalinei <remus.amalinei@gmail.com>
2015-10-19 12:57:02 +0100

    Add actor and domain service for sorting scores
    Project: top-ranker

---
+++ top-ranker/src/main/scala/ra/topranker/domain/service/ScoreSorterService.scala
@@ -0,0 +1,13 @@
+package ra.topranker.domain.service
+
+import ra.topranker.domain.Score
+
+/**
+ * @author Remus Amalinei
+ */
+trait ScoreSorterService {
+
+  def addScore(score: Score): Unit
+
+  def retrieveTopScores(): IndexedSeq[Score]
+}
---
+++ top-ranker/src/test/scala/ra/topranker/actor/TopRankerActorTest.scala
@@ -0,0 +1,58 @@
+package ra.topranker.actor
+
+import ra.topranker.actor.TopRankerActor.{AddScore, QueryTopScores, TopScores}
+import ra.topranker.application.TopRankerService
+import ra.topranker.domain.service.ScoreSorterService
+import ra.topranker.domain.{Player, Score}
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit}
+import org.junit.runner.RunWith
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+/**
+ * @author Remus Amalinei
+ */
+@RunWith(classOf[JUnitRunner])
+class TopRankerActorTest(_system: ActorSystem)
+  extends TestKit(_system)
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll
+  with MockFactory {
+
+  def this() = this(ActorSystem("test"))
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "TopRankerActor should" should {
+    "call the ScoreSorterService to add a new Score" in {
+      val scoreSorterServiceStub = stub[ScoreSorterService]
+      val topRankerActor = system.actorOf(Props(classOf[TopRankerActor], scoreSorterServiceStub))
+      val testScore = Score(Player("test"), 1)
+
+      topRankerActor ! AddScore(testScore)
+
+      (scoreSorterServiceStub.addScore _).verify(testScore)
+    }
+  }
+
+  it should {
+    "call the ScoreSorterService to retrieve the top scores" in {
+      val scoreSorterServiceMock = mock[ScoreSorterService]
+      val expectedScores = Vector(
+        Score(Player("a"), 2),
+        Score(Player("b"), 1))
+      (scoreSorterServiceMock.retrieveTopScores _).expects().returning(expectedScores)
+
+      val topRankerActor = system.actorOf(Props(classOf[TopRankerActor], scoreSorterServiceMock))
+
+      topRankerActor ! QueryTopScores()
+      expectMsg(TopScores(expectedScores))
+    }
+  }
+}
